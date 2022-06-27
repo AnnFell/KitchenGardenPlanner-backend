603763ddf3bf5ac1d425cd394d574e2d68b90dd4
Annelotte Fellinger <annelottef@gmail.com>
2022-06-27 14:18:37 +0200

    Initial commit

---
+++ src/main/java/com/example/KitchenGardenPlanner/KitchenGardenPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.KitchenGardenPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenGardenPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenGardenPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KitchenGardenPlanner/KitchenGardenPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KitchenGardenPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenGardenPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
